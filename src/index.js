aa0090796f24dfd3d3c31d379f0e109a3549c25f
Gabriel Prange <prange.gabriel@gmail.com>
2022-06-24 17:54:17 -0300

    primeiro middleware

--- src/index.js
+++ src/index.js
@@ -7,13 +7,21 @@ app.use(express.json());
 
 const customers = [];
 
-/**
- * tipos de dados:
- * cpf - string
- * name - string
- * id - uuid
- * statement []
- **/
+//Middleware
+function verifyIfExistAccoutCPF(request, response, next) {
+  const { cpf } = request.headers;
+
+  const customer = customers.find((customer) => customer.cpf === cpf);
+
+  if (!customer) {
+    return response.status(400).json({ error: "Customer not found" });
+  }
+
+  request.customer = customer;
+
+  return next();
+}
+
 app.post("/account", (request, response) => {
   const { cpf, name } = request.body;
 
@@ -35,14 +43,8 @@ app.post("/account", (request, response) => {
   return response.status(201).send();
 });
 
-app.get("/statement", (request, response) => {
-  const { cpf } = request.headers;
-
-  const customer = customers.find((customer) => customer.cpf === cpf);
-
-  if (!customer) {
-    return response.status(400).json({ error: "Customer not found" });
-  }
+app.get("/statement", verifyIfExistAccoutCPF, (request, response) => {
+  const { customer } = request;
 
   return response.json(customer.statement);
 });
